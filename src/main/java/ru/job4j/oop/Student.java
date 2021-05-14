8ae6b93bad49a6196fe11a6ada7cc139b33408cc
WilliamMad <gleb.lipai@yandex.ru>
2021-05-14 11:42:55 +0300

    1.2. Вызов метода объекта [#166085 #1346]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -5,6 +5,10 @@ public class Student {
         System.out.println("Tra tra tra");
     }
 
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
@@ -13,9 +17,5 @@ public class Student {
         petya.song();
         petya.song();
         petya.song();
-    }
-
-    public void song() {
-        System.out.println("I believe I can fly");
     }
 }
