f23e49fe8f146191602684cba21c46b7690ee169
WilliamMad <gleb.lipai@yandex.ru>
2021-05-17 17:46:09 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,6 +5,11 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 
     public int getId() {
         return id;
