8b24254abce0548b272360badf1ee270ea4e8cf5
glebl <gleb.lipai@yandex.ru>
2021-05-17 18:35:28 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,23 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {}
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 
     public LocalDateTime getCreated() {
         return created;
@@ -27,16 +39,4 @@ public class Item {
         this.name = name;
     }
 
-    public Item() {
-
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
 }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,8 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        item.getCreated();
-        System.out.println("Текущие дата и время после форматирования: " + item.getCreated());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println("Текущие дата и время после форматирования: " + item.getCreated().format(formatter));
     }
 }
 
