c2ec73cff2e77c51e380728a14e12dbda535d245
glebl <gleb.lipai@yandex.ru>
2021-05-18 21:09:32 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,6 +8,8 @@ public class StartUI {
         Tracker tracker = new Tracker();
         tracker.add(new Item("Something", 1));
         System.out.println(tracker.findById(1));
+        Item item = new Item("Anything", 2);
+        System.out.println(item);
     }
 }
 
