7f1f00ebd8f8695ab375929fe37a82c9187e0fad
WilliamMad <gleb.lipai@yandex.ru>
2021-05-20 16:41:15 +0300

    3. Интерфейс Input [#181092 #158561]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -2,10 +2,22 @@ package ru.job4j.poly;
 
 import java.util.Scanner;
 
-public class Bus implements Vehicle {
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        System.out.println("Еду");
+    }
 
     @Override
-    public void move() {
-        System.out.println(getClass().getSimpleName() + " Едет по дороге");
+    public void passengers(int number) {
+        System.out.println(number);
+    }
+
+    @Override
+    public int refuel(int liters) {
+        Scanner sc = new Scanner(System.in);
+        liters = sc.nextInt();
+        int price = 50;
+        return liters * price;
     }
 }
