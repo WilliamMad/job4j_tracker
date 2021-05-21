eef2804632df692ce1ddbbe4931d252ab99f1466
glebl <gleb.lipai@yandex.ru>
2021-05-21 21:18:19 +0300

    9.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,11 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
@@ -22,14 +27,15 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(), new ShowAllAction(), new EditAction(),
-                new DeleteAction(), new FindIdAction(), new FindNameAction(),
+                new CreateAction(output), new ShowAllAction(output), new EditAction(output),
+                new DeleteAction(output), new FindIdAction(output), new FindNameAction(output),
                 new Exit()
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
     }
 }
 
