173e37036fed844b3d3061834971f35354be95cb
glebl <gleb.lipai@yandex.ru>
2021-06-07 22:56:13 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import java.util.Comparator;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByNameRev().thenComparing(new JobDescByPriorityRev());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+}
