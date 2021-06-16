6f1a59ced4fa8056d3961fe8da61635f792c07f1
WilliamMad <gleb.lipai@yandex.ru>
2021-06-16 17:13:22 +0300

    Fixing

--- src/main/java/ru/job4j/stream/StreamUse.java
+++ src/main/java/ru/job4j/stream/StreamUse.java
@@ -3,7 +3,6 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StreamUse {
     public static void main(String[] args) {
