2ca929bb09eb924ad255134b64aa6cf8ba05fc8c
WilliamMad <gleb.lipai@yandex.ru>
2021-06-16 16:50:40 +0300

    0.1. Отладка лямбда в IDEA [#259165]

---
+++ src/main/java/ru/job4j/stream/StreamUse.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamUse {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(3);
+        nums.add(-5);
+        nums.add(6);
+        nums.add(-1);
+        List<Integer> positive = nums.stream().filter(
+                num -> num > 0).collect(Collectors.toList());
+        positive.forEach(System.out::println);
+    }
+}
