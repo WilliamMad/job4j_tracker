a2dc1d75069e456c297082a0af5436dd427b9ac4
glebl <gleb.lipai@yandex.ru>
2021-06-21 20:07:26 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,10 +1,18 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+    }
+
+    public List<Address> cutDups(List<Profile> profiles) {
+        return collect(profiles).stream()
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -3,6 +3,7 @@ package ru.job4j.stream;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -20,5 +21,21 @@ public class ProfilesTest {
         List<Profile> profiles = List.of(prs1, prs2);
         List<Address> expected = List.of(adrs1, adrs2);
         assertThat(new Profiles().collect(profiles), is(expected));
+    }
+
+    @Test
+    public void collectsCutDups() {
+        Address adrs1 = new Address("Saint-Petersburg", "blvr Trydyashihsa", 2, 100);
+        Address adrs2 = new Address("Moscow", "Usacheva street", 1, 1);
+        Profile prs1 = Profile.of(adrs1);
+        Profile prs2 = Profile.of(adrs2);
+        Profile prs3 = Profile.of(adrs1);
+        Profile prs4 = Profile.of(adrs2);
+        List<Profile> profiles = List.of(prs1, prs2, prs3, prs4);
+        List<Address> expected = new ArrayList<>();
+        expected.add(adrs1);
+        expected.add(adrs2);
+        expected.sort(Comparator.comparing(Address::getCity));
+        assertThat(new Profiles().cutDups(profiles), is(expected));
     }
 }
