071dcd010aa2aa61f11b407a386d0aa76f18f842
WilliamMad <gleb.lipai@yandex.ru>
2021-06-25 15:01:23 +0300

    Checkstyle

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -1,13 +1,11 @@
 package ru.job4j.collection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList();
-        names.add("Petr");
-        names.add("Ivan");
-        names.add("Stepan");
+        List<String> names = List.of("Petr", "Ivan", "Stepan");
         for (String value : names) {
             System.out.println(value);
         }
